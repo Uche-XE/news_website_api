85d2a70211a70c03a006dfea43aa11931ed7e9c0
0327-07 <utchaynwaogu@gmail.com>
2022-03-09 18:10:57 +0100

    news_api_react

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {BiCopyright} from "react-icons/bi";
+
+const Footer = ({footerDate}) => {
+  return (
+    <footer>
+<span>{BiCopyright}</span>
+<p>{footerDate.current} © Uche Nwaogu</p>
+
+    </footer>
+  )
+}
+
+export default Footer
